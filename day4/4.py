1ec1d5301506be4a5eba50252d696c2d70660073
Supi <126416259+Sooperu@users.noreply.github.com>
2023-02-26 18:37:10 -0600

    this time its really done

--- day4/4.py
+++ day4/4.py
@@ -2,7 +2,7 @@ import json
 from typing import List
 import shared
 #Imports a method that pulls the text data from the AOC site
-org_list = shared.get_day(4)
+org_list = shared.get_day(2022, 4)
 
 #Part 1
 count = 0
--- day5/5.py
+++ day5/5.py
@@ -2,5 +2,5 @@ import json
 from typing import List
 import shared
 
-org_list = shared.get_day(5)
+org_list = shared.get_day(2022, 5)
 print(json.dumps(org_list))
